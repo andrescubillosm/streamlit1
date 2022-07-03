6f3d8acd2efcf151c4de5bf03512a3a666d4f28a
carlo <carlos.cubillos@urosario.edu.co>
2022-07-03 15:49:12 -0500

    8

--- Eficacia_main_st.py
+++ Eficacia_main_st.py
@@ -144,20 +144,20 @@ st.sidebar.markdown(" :copyright: 2022 &copy;")
 #url_2 = 'https://eficaciadata.s3.amazonaws.com/pro_pre_pdv.csv' # Data of products-Prices-stores
 #url_3 = 'https://eficaciadata.s3.amazonaws.com/pre.csv'# Data of prices
 #url_4 = 'https://eficaciadata.s3.amazonaws.com/pro.csv' # DATA DE PRODUCTOS
-#url_5 = 'https://eficaciadata.s3.amazonaws.com/ago_pdv_pro.csv' # Data of products out stock for store and type of product
-#url_6 = 'https://eficaciadata.s3.amazonaws.com/ago.csv' # DATA DE AGOTADOS
+url_5 = 'https://eficaciadata.s3.amazonaws.com/ago_pdv_pro.csv' # Data of products out stock for store and type of product
+url_6 = 'https://eficaciadata.s3.amazonaws.com/ago.csv' # DATA DE AGOTADOS
 
 url_1 = 'csv/geodata.csv' # Data of geographic points of the stores
 url_2 = 'csv/pro_pre_pdv.csv' # Data of products-Prices-stores
 url_3 = 'csv/pre.csv'# Data of prices
 url_4 = 'csv/pro.csv' # DATA DE PRODUCTOS
-url_5 = 'csv/ago_pdv_pro.csv' # Data of products out stock for store and type of product
-url_6 = 'csv/ago.csv' # DATA DE AGOTADOS
+#url_5 = 'csv/ago_pdv_pro.csv' # Data of products out stock for store and type of product
+#url_6 = 'csv/ago.csv' # DATA DE AGOTADOS
 #uses this instruccion it the data change @st.cache(persist=True)( If you have a different use case where the data does not change so very often, you can simply use this)
 
 
 #loading data
-gea = pd.read_csv(url_1)
+gea = pd.read_csv(url_1, sep=';')
 data_2 = pd.read_csv(url_2)
 data_3 = pd.read_csv(url_3)
 data_4 = pd.read_csv(url_4)
