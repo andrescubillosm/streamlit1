0d9df7a4a8ff02dc54e315b68c452cf022df1d51
carlo <carlos.cubillos@urosario.edu.co>
2022-07-03 15:51:12 -0500

    3

--- Eficacia_main_st.py
+++ Eficacia_main_st.py
@@ -140,19 +140,19 @@ st.sidebar.markdown("Developed by team 108 :globe_with_meridians: for DS4A Colom
 st.sidebar.markdown(" :copyright: 2022 &copy;")
 
 #Adding datasets
-#url_1 = 'https://eficaciadata.s3.amazonaws.com/geodata.csv' # Data of geographic points of the stores
+url_1 = 'https://eficaciadata.s3.amazonaws.com/geodata.csv' # Data of geographic points of the stores
 #url_2 = 'https://eficaciadata.s3.amazonaws.com/pro_pre_pdv.csv' # Data of products-Prices-stores
 #url_3 = 'https://eficaciadata.s3.amazonaws.com/pre.csv'# Data of prices
 #url_4 = 'https://eficaciadata.s3.amazonaws.com/pro.csv' # DATA DE PRODUCTOS
-url_5 = 'https://eficaciadata.s3.amazonaws.com/ago_pdv_pro.csv' # Data of products out stock for store and type of product
-url_6 = 'https://eficaciadata.s3.amazonaws.com/ago.csv' # DATA DE AGOTADOS
+#url_5 = 'https://eficaciadata.s3.amazonaws.com/ago_pdv_pro.csv' # Data of products out stock for store and type of product
+#url_6 = 'https://eficaciadata.s3.amazonaws.com/ago.csv' # DATA DE AGOTADOS
 
 url_1 = 'csv/geodata.csv' # Data of geographic points of the stores
 url_2 = 'csv/pro_pre_pdv.csv' # Data of products-Prices-stores
 url_3 = 'csv/pre.csv'# Data of prices
 url_4 = 'csv/pro.csv' # DATA DE PRODUCTOS
-#url_5 = 'csv/ago_pdv_pro.csv' # Data of products out stock for store and type of product
-#url_6 = 'csv/ago.csv' # DATA DE AGOTADOS
+url_5 = 'csv/ago_pdv_pro.csv' # Data of products out stock for store and type of product
+url_6 = 'csv/ago.csv' # DATA DE AGOTADOS
 #uses this instruccion it the data change @st.cache(persist=True)( If you have a different use case where the data does not change so very often, you can simply use this)
 
 
@@ -371,4 +371,3 @@ with map_dash:
 #    st_map =st_folium(map_tiendas, width=600)
 
 #st.write(mapa)
-
