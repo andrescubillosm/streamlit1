f54511289035464e00c77f79c35f31747aba499c
carlo <carlos.cubillos@urosario.edu.co>
2022-07-03 22:01:12 -0500

    ultimo ensayo

--- Eficacia_main_st.py
+++ Eficacia_main_st.py
@@ -153,19 +153,19 @@ st.sidebar.write(f'''
 st.sidebar.markdown(" &copy; 2022 &copy;")
 
 #Adding datasets
-#url_1 = 'https://eficaciadata.s3.amazonaws.com/geodata.csv' # Data of geographic points of the stores
+url_1 = 'https://eficaciadata.s3.amazonaws.com/geodata.csv' # Data of geographic points of the stores
 url_2 = 'https://eficaciadata.s3.amazonaws.com/pro_pre_pdv.csv' # Data of products-Prices-stores
-#url_3 = 'https://eficaciadata.s3.amazonaws.com/pre.csv'# Data of prices
-#url_4 = 'https://eficaciadata.s3.amazonaws.com/pro.csv' # DATA DE PRODUCTOS
+url_3 = 'https://eficaciadata.s3.amazonaws.com/pre.csv'# Data of prices
+url_4 = 'https://eficaciadata.s3.amazonaws.com/pro.csv' # DATA DE PRODUCTOS
 url_5 = 'https://eficaciadata.s3.amazonaws.com/ago_pdv_pro.csv' # Data of products out stock for store and type of product
-#url_6 = 'https://eficaciadata.s3.amazonaws.com/ago.csv' # DATA DE AGOTADOS
+url_6 = 'https://eficaciadata.s3.amazonaws.com/ago.csv' # DATA DE AGOTADOS
 
-url_1 = 'csv/geodata1.csv' # Data of geographic points of the stores
+#url_1 = 'csv/geodata1.csv' # Data of geographic points of the stores
 #url_2 = 'csv/pro_pre_pdv.csv' # Data of products-Prices-stores
-url_3 = 'csv/pre.csv'# Data of prices
-url_4 = 'csv/pro.csv' # DATA DE PRODUCTOS
+#url_3 = 'csv/pre.csv'# Data of prices
+#url_4 = 'csv/pro.csv' # DATA DE PRODUCTOS
 #url_5 = 'csv/ago_pdv_pro.csv' # Data of products out stock for store and type of product
-url_6 = 'csv/ago.csv' # DATA DE AGOTADOS
+#url_6 = 'csv/ago.csv' # DATA DE AGOTADOS
 #uses this instruccion it the data change @st.cache(persist=True)( If you have a different use case where the data does not change so very often, you can simply use this)
 
 
@@ -282,7 +282,7 @@ with stats_dash:
 
 #giving format to coordinates
 gen_map = gea
-gea_2= gea.loc[gea["Ciudad"] == select_ciudad]
+gea_2 = gea.loc[gea["Ciudad"] == select_ciudad]
 gea_2["Lat"] = gea_2["Lat"].apply(lambda x: x.replace(',', '.'))
 gea_2["Lon"] = gea_2["Lon"].apply(lambda x: x.replace(',', '.'))
 gea_2['Lat'] = gea_2['Lat'].astype(float)
